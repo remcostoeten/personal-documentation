b6f9fbb15a25eb08fc1e242cfbfd6aec659ac380
Remco Stoeten <remcostoeten@hotmail.com>
2024-11-08 16:57:01 +0100

    feat: setup config files and move over fonts/metadata

--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 
 A (WiP, to be) personal documentation site without any documentation framework, completely custom with the goal of storing snippets, components, hooks which I reuse across different project for personal reference, and who knows, someone else who finds the component cool.
 
-
 xxx
 
 Remco Stoeten
---
+++ src/core/config/site.ts
@@ -0,0 +1,17 @@
+export const siteConfig = {
+	name: 'Remco Stoeten | Personal documentation',
+	url: 'https://docs.remcostoeten.com',
+	description: 'A custom documentation site build in NextJS to store my own re-usable componetns',
+	author: {
+		name: 'Remco Stoeten',
+        url: 'https://remcostoeten.com',
+	},
+	socials: {
+		github: 'https://github.com/remcostoeten',
+		discord: 'https://discordapp.com/users/635917960359444490',
+		x: 'https://twitter.com/yowremco'
+	},
+	usernames: 'remcostoeten'
+}
+
+export type SiteConfig = typeof siteConfig
