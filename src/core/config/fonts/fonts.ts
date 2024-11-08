9b239c08ac7200989811ecdc8da5ce15509bd022
Remco <remcostoeten@hotmail.com>
2024-11-08 17:31:51 +0100

    chore: create folder structure and init shadcn (#2)

--- src/core/config/fonts/fonts.ts
+++ src/core/config/fonts/fonts.ts
@@ -5,13 +5,13 @@ import localFont from 'next/font/local'
  * -----------------------------------------------------------------------------------------------*/
 
 export const geistSans = localFont({
-    src: './GeistVF.woff',
-    variable: '--font-geist-sans',
-    weight: '100 900'
+	src: './GeistVF.woff',
+	variable: '--font-geist-sans',
+	weight: '100 900'
 })
 
 export const geistMono = localFont({
-    src: './GeistMonoVF.woff',
-    variable: '--font-geist-mono',
-    weight: '100 900'
+	src: './GeistMonoVF.woff',
+	variable: '--font-geist-mono',
+	weight: '100 900'
 })
--- src/core/config/site.ts
+++ src/core/config/site.ts
@@ -1,10 +1,11 @@
 export const siteConfig = {
 	name: 'Remco Stoeten | Personal documentation',
 	url: 'https://docs.remcostoeten.com',
-	description: 'A custom documentation site build in NextJS to store my own re-usable componetns',
+	description:
+		'A custom documentation site build in NextJS to store my own re-usable componetns',
 	author: {
 		name: 'Remco Stoeten',
-        url: 'https://remcostoeten.com',
+		url: 'https://remcostoeten.com'
 	},
 	socials: {
 		github: 'https://github.com/remcostoeten',
